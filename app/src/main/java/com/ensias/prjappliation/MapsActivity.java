86ec6ce807f0a920a76043774826f41ad58f32bc
mohammed-eddaghal <mohammed_eddaghal@um5.ac.ma>
2021-04-20 22:39:21 +0000

    new search bar

--- app/src/main/java/com/ensias/prjappliation/MapsActivity.java
+++ app/src/main/java/com/ensias/prjappliation/MapsActivity.java
@@ -2,7 +2,11 @@ package com.ensias.prjappliation;
 
 import androidx.fragment.app.FragmentActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -14,11 +18,18 @@ import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.mancj.materialsearchbar.MaterialSearchBar;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
+    private static final String[] tolsName={"tornouvise","tomobile","cup","coutant"};
+
+    AutoCompleteTextView autoCompleteTextView;
+
     private GoogleMap mMap;
     String userPath="users";
+
+    MaterialSearchBar materialSearchBar;
 
     // creating a variable for our
     // Firebase Database.
@@ -45,6 +56,52 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         firebaseDatabase = FirebaseDatabase.getInstance();
         // below line is used to get reference for our database.
         databaseReference = firebaseDatabase.getReference(userPath);
+
+        autoCompleteTextView = findViewById(R.id.searchBar);
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, tolsName);
+        autoCompleteTextView.setAdapter(arrayAdapter);
+        String inputValue=autoCompleteTextView.getText().toString();
+
+        Toast.makeText(MapsActivity.this,inputValue,Toast.LENGTH_SHORT).show();
+
+        /*materialSearchBar.setOnSearchActionListener(new MaterialSearchBar.OnSearchActionListener() {
+            @Override
+            public void onSearchStateChanged(boolean enabled) {
+
+            }
+
+            @Override
+            public void onSearchConfirmed(CharSequence text) {
+                startSearch(text.toString(),true,null,true);
+            }
+
+            @Override
+            public void onButtonClicked(int buttonCode) {
+                if(buttonCode == MaterialSearchBar.BUTTON_NAVIGATION  ){
+                    //open or close navigation bare
+                }else if(buttonCode == MaterialSearchBar.BUTTON_BACK){
+                    materialSearchBar.disableSearch();
+                }
+
+            }
+        });
+
+        materialSearchBar.addTextChangeListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });*/
     }
 
     /**
