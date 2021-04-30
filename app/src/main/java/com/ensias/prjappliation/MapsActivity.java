e71c124068046d41d586d6141563919c90302d25
mohammed-eddaghal <mohammed_eddaghal@um5.ac.ma>
2021-04-30 17:23:47 +0000

    commit

--- app/src/main/java/com/ensias/prjappliation/MapsActivity.java
+++ app/src/main/java/com/ensias/prjappliation/MapsActivity.java
@@ -5,8 +5,11 @@ import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
+import android.widget.SearchView;
 import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -20,16 +23,18 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.mancj.materialsearchbar.MaterialSearchBar;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+import java.util.ArrayList;
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback, SearchView.OnQueryTextListener {
 
     private static final String[] tolsName={"tornouvise","tomobile","cup","coutant"};
 
-    AutoCompleteTextView autoCompleteTextView;
+
 
     private GoogleMap mMap;
     String userPath="users";
 
-    MaterialSearchBar materialSearchBar;
+
 
     // creating a variable for our
     // Firebase Database.
@@ -42,10 +47,16 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     //instance of user classe
     User user;
 
+    SearchView editsearch;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
+
+        editsearch = (SearchView) findViewById(R.id.search);
+        editsearch.setOnQueryTextListener(this);
+
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
@@ -57,51 +68,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         // below line is used to get reference for our database.
         databaseReference = firebaseDatabase.getReference(userPath);
 
-        autoCompleteTextView = findViewById(R.id.searchBar);
-        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, tolsName);
-        autoCompleteTextView.setAdapter(arrayAdapter);
-        String inputValue=autoCompleteTextView.getText().toString();
 
-        Toast.makeText(MapsActivity.this,inputValue,Toast.LENGTH_SHORT).show();
-
-        /*materialSearchBar.setOnSearchActionListener(new MaterialSearchBar.OnSearchActionListener() {
-            @Override
-            public void onSearchStateChanged(boolean enabled) {
-
-            }
-
-            @Override
-            public void onSearchConfirmed(CharSequence text) {
-                startSearch(text.toString(),true,null,true);
-            }
-
-            @Override
-            public void onButtonClicked(int buttonCode) {
-                if(buttonCode == MaterialSearchBar.BUTTON_NAVIGATION  ){
-                    //open or close navigation bare
-                }else if(buttonCode == MaterialSearchBar.BUTTON_BACK){
-                    materialSearchBar.disableSearch();
-                }
-
-            }
-        });
-
-        materialSearchBar.addTextChangeListener(new TextWatcher() {
-            @Override
-            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-
-            }
-
-            @Override
-            public void onTextChanged(CharSequence s, int start, int before, int count) {
-
-            }
-
-            @Override
-            public void afterTextChanged(Editable s) {
-
-            }
-        });*/
     }
 
     /**
@@ -129,7 +96,16 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         }*/
         // Add a marker in Sydney and move the camera
         LatLng mark = new LatLng(lat, lang);
-        mMap.addMarker(new MarkerOptions().position(mark).title("Marker in Sydney"));
+
+
+
+        mMap.addMarker(new MarkerOptions().position(mark).title("my location"));
+
+        for(int i = 0;i<10;i++){
+            double x = Math.random()/100;
+            double y = Math.random()/100;
+            mMap.addMarker(new MarkerOptions().position(new LatLng(lat+x,lang+y)).title("Marker in Sydney"));
+        }
 
         mMap.moveCamera(CameraUpdateFactory.newLatLng(mark));
 
@@ -139,5 +115,16 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mMap.getUiSettings().setCompassEnabled(true);
         mMap.getUiSettings().setZoomControlsEnabled(true);
 
+    }
+
+    @Override
+    public boolean onQueryTextSubmit(String query) {
+        Toast.makeText(MapsActivity.this,query,Toast.LENGTH_LONG).show();
+        return false;
+    }
+
+    @Override
+    public boolean onQueryTextChange(String newText) {
+        return false;
     }
 }
